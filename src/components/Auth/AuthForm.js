76daedcc6d57305d89087a834a063d0c22c004c7
Faisal <md.faisal1899@yahoo.com>
2022-07-30 08:28:40 +0600

    added sign up functionality

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -13,10 +13,44 @@ const AuthForm = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (event) => {
+
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passInputRef.current.value;
+
+    if(isLogin){
+
+    }else{
+      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBnQPCpSKz7qOXOF7nULTvvCQJ8_na-KEo', {
+        method: 'POST',
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then(res => {
+        if(res.ok){
+
+        }else{
+          return res.json().then(data => {
+            console.log(data);
+          })
+        }
+      });
+    }
+  };
+
+
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form onSubmit={}>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
           <input type='email' id='email' required ref={emailInputRef} />
