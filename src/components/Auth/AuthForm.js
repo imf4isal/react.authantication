afa82afeb77896958991b3c28f88faec0a3d604c
Faisal <md.faisal1899@yahoo.com>
2022-07-30 08:28:15 +0600

    started authentication project

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,43 @@
+import { useRef, useState } from 'react';
+
+import classes from './AuthForm.module.css';
+
+const AuthForm = () => {
+
+  const [isLogin, setIsLogin] = useState(true);
+
+  const emailInputRef = useRef();
+  const passInputRef = useRef();
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form onSubmit={}>
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' required ref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input type='password' id='password' required ref={passInputRef} />
+        </div>
+        <div className={classes.actions}>
+          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button
+            type='button'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
